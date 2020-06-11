edbebb161b4206fd9d75b88abf9aeba98bb30824
Andreas Heizmann <ah@interap.de>
2020-06-11 13:34:59 +0200

    ..

--- webviewtest.js
+++ webviewtest.js
@@ -1,3 +1,7 @@
 var onLogoutClicked = function(event) {
     window.location.href = "https://google.de";
 }
+
+var onExternalClicked = function(event) {
+    window.location.href = "XXXXX://RUN.htm?cmdName=Method1&param=123";
+}
